75763a385839b43216975f9bcf638a8299799b59
Alexey <Alexey@DESKTOP-7JVS5MF>
2022-06-13 20:49:15 +0300

    Add project files.

---
+++ Training.Dergai.Lesson2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Math;
+
+namespace Training.Dergai.Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunCalculateRoot();
+        }
+        private static void RunCalculateRoot()
+        {
+            Console.WriteLine("ROOT CALCULATION ( Степень корня: {0}, Число под корнем: {1}) = {2}", 2, 3, RootCalculations.CalculateRoot(2, 3));
+        }
+    }
+}
---
+++ Training.Dergai.Lesson2/RootCalculations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Training.Dergai.Lesson2
+{
+    public static class RootCalculations
+    {
+        public static double CalculateRoot(double x, int N)
+        {
+            x = 1;
+            double eps = 1e-15;
+
+            for (; ; )
+            {
+                double nx = (x + N / x) / 2;
+                if (Math.Abs(x - nx) < eps) break;
+                x = nx;
+            }
+            return x;
+        }
+    }
+}
