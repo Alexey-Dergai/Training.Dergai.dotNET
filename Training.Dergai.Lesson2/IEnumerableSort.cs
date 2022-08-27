927aa4ceb089a1243c527acc724733075659e63b
Alexey Dergai <al.dergai@mail.ru>
2022-08-27 14:18:09 +0300

    Add IEnumerableSort file and Implement generic sort-method for IEnumerable<T>

--- Training.Dergai.Lesson2/IEnumerableSort.cs
+++ Training.Dergai.Lesson2/IEnumerableSort.cs
@@ -6,11 +6,30 @@ using System.Threading.Tasks;
 
 namespace Training.Dergai.Lesson2
 {
-    class IEnumerableSort
+    public static class SortExtension
     {
-        public static void sos()
+        public enum SortDirection
         {
+            Ascending,
+            Descending
+        }
+        // TODO: add enum with sort direction
+        public static IEnumerable<T> Sort<T>(this IEnumerable<T> collection, SortDirection sortDirection) where T : IComparable<T>
+        {
+            var sortedCollection = collection.ToList();
 
+            for (var i = 0; i < sortedCollection.Count; i++)
+            {
+                for (var j = i + 1; j < sortedCollection.Count; j++)
+                {
+                    if ((sortDirection == SortDirection.Ascending && sortedCollection[i].CompareTo(sortedCollection[j]) > 0) || (sortDirection == SortDirection.Descending && sortedCollection[i].CompareTo(sortedCollection[j]) < 0))
+                    {
+                        (sortedCollection[i], sortedCollection[j]) = (sortedCollection[j], sortedCollection[i]);
+                    }
+                }
+            }
+
+            return sortedCollection;
         }
     }
 }
